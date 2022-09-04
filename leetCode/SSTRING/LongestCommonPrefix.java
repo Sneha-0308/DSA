f86c94696b723fe102839294a0bfd7cce9853c70
Sneha-0308 <mathadawar01@gmail.com>
2022-09-04 11:16:14 +0530

    Day 34

--- leetCode/SSTRING/LongestCommonPrefix.java
+++ leetCode/SSTRING/LongestCommonPrefix.java
@@ -2,32 +2,24 @@ package com.leetCode.SSTRING;
 
 public class LongestCommonPrefix {
     static String longestCommonPrefix(String[] strs) {
-        String res="";
-        char temp;
-        int flag=0;
-        if(strs.length==1){
-            return strs[0];
-        }
-        for(int i=0;i<strs[0].length();i++){
-            temp=strs[0].charAt(i);
-            for(int j=1;j<strs.length;j++){
-                if(i<strs[j].length()){
-                    if(strs[j].charAt(i)==temp)
-                        flag=1;
-                    else
-                        flag=0;
-                }
-                else
-                    break;
+       // if the array does not contain any String the return empty String
+        if(strs.length==0)
+            return "";
+//        consider first word in string as prefix
+        String prefix=strs[0];
+//        loop till the array length
+        for(int i=0;i<strs.length;i++){
+//            every time consider the each word and check whether that prefix is present in that word
+//            if the answer is 0 then it is present else reduce the prefix by one letter and also check
+            while(strs[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
+//                once you get the prefix check whether value is empty if it is empty that means
+//                we dont have any prefix;
+                if(prefix.isEmpty())
+                    return "";
             }
-            if(flag==1) {
-                res += temp;
-                flag=0;
-            }
-            else
-                break;
         }
-        return res;
+        return prefix;
     }
     public static void main(String[] args){
         String[] strs={"aaa","aa","aaa"};
