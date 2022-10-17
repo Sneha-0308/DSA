1af730ee7dcb89ef4f7de022be0fd827079f94dd
Sneha-0308 <mathadawar01@gmail.com>
2022-10-17 06:11:25 +0530

    Day 59

--- leetCode/SSTRING/LongestPalindrome.java
+++ leetCode/SSTRING/LongestPalindrome.java
@@ -1,35 +1,28 @@
 package com.leetCode.SSTRING;
 
+import java.util.HashSet;
+
 public class LongestPalindrome {
-    static boolean isPalindrome(String str){
-        int i=0;
-        int j=str.length()-1;
-        while (i<j){
-            if(str.charAt(i)!=str.charAt(j))
-                return false;
-            i++;j--;
+    static int func(String s){
+        HashSet<Character> set = new HashSet<>();
+        int count=0;
+        if(s.length()==0 || s==null)
+            return 0;
+        for(int i=0;i<s.length();i++){
+            if(set.contains(s.charAt(i))){
+                set.remove(s.charAt(i));
+                count++;
+            }
+            else
+                set.add(s.charAt(i));
         }
-        return true;
+        if(!set.isEmpty())
+            return count*2+1;
+        else
+            return count*2;
     }
 
-    static int longestPalindrome(String s){
-        int result=0;
-        String sub="";
-        for(int i=0;i<s.length();i++){
-            for(int j=s.length()-1;j>=0;j--){
-                sub=s.substring(i,j+1);
-                if(result>sub.length())
-                    break;
-                else{
-                    if(isPalindrome(sub)){
-                        result=sub.length();
-                    }
-                }
-            }
-        }
-        return result;
-    }
-    public static void main(String[] args){
-        System.out.println(longestPalindrome("abadd"));
+    public static void main(String[] args) {
+        System.out.println(func("abcccdd"));
     }
 }
